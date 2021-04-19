519681b5fdf427e93adcfeafa37d86f454b6cbec
Oliver Johansson <oliverjohansson3@gmail.com>
2021-04-19 14:20:29 +0200

    Boilerplate for userroutes and postsroutes

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,13 @@
-const express = require('express');
-
+import express from 'express'
+const userRouter = require('./routes/users.routes.ts')
+const postRouter = require('./routes/posts.routes.ts')
 const app = express();
 
 const PORT = 4000;
+
+app.use(express.json())
+app.use('/api/users', userRouter)
+app.use('/api/posts', postRouter)
 
 app.get("/", (_: any, res: any) => res.send("Hello from server!"));
 
