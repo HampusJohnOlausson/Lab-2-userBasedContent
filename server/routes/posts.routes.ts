de2610ffb03e6193b6e6f2ae32e7496ffaa9466b
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-21 09:34:59 +0200

    added tweet.ts and connected to database

--- server/routes/posts.routes.ts
+++ server/routes/posts.routes.ts
@@ -1,87 +1,64 @@
 import express from 'express'
 import { readBuilderProgram } from 'typescript';
 const router = express.Router()
+import Tweet from '../models/tweet.js';
 
-let tweets = [
-  {
-    id: 1,
-    tweet: " tweet 1",
-  },
-  {
-    id: 2,
-    tweet: " tweet 2",
-  },
-];
 
 router.get('/tweets', ( req, res) => {
-    if(!tweets){
-        return res.status(404).send('Sorry Couldnt fins the tweets');
-    }
-    res.send(tweets);
+    Tweet.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.status(200).json(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 })
 
 
 router.get("/tweets/:id", (req, res) => {
-    const tweetId = req.params.id;
-    const tweet = tweets.find(t => t.id === parseInt(tweetId));
-
-    if(!tweet){
-        return res.status(404).send('Sorry, the tweet was not found!');
-    }
-    res.send(tweet);
+    const id = req.params.id;
+    Tweet.findById(id)
+      .then((result) => {
+        res.status(200).json(result)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    
 });
 
 router.post("/tweets/", (req, res) => {
 
-    if(!req.body.tweet){
-        return res.status(400).send('You need to tweet something!')
-    }
-    
-     let newId = 0;
-     tweets.forEach((tweet) => {
-       if (tweet.id > newId) {
-         newId = tweet.id;
-       }
-     });
-     newId++;
+  const tweet = new Tweet(req.body);
+  tweet.save()
 
-     const tweet = req.body;
-
-     const newTweet = {
-       id: newId,
-       tweet: tweet,
-     };
-
-     tweets.push(newTweet);
-
-     res.status(201).send(newTweet.tweet);
 })
 
 router.delete('/tweets/:id', (req, res) => {
 
-    const tweetId = req.params.id;
-    tweets = tweets.filter((tweet) => tweet.id !== parseInt(tweetId));
+    // const tweetId = req.params.id;
+    // tweets = tweets.filter((tweet) => tweet.id !== parseInt(tweetId));
 
-    if(!tweetId){
-        return res.status(404).send('couldnt find the tweet');
-    }
+    // if(!tweetId){
+    //     return res.status(404).send('couldnt find the tweet');
+    // }
 
-    res.send('The tweet was deleted');
+    // res.send('The tweet was deleted');
 })
 
 router.put('/tweets/:id', (req, res) => {
-    const tweetId = req.params.id;
-    const tweet = tweets.find((t) => t.id === parseInt(tweetId));
+    // const tweetId = req.params.id;
+    // const tweet = tweets.find((t) => t.id === parseInt(tweetId));
 
-    if(!tweet){
-        return res.status(404).send('The tweet was not found!');
-    }
+    // if(!tweet){
+    //     return res.status(404).send('The tweet was not found!');
+    // }
 
-    tweet.tweet = req.body.tweet;
+    // tweet.tweet = req.body.tweet;
 
-    res.status(200).send('the tweet has been updated!');
+    // res.status(200).send('the tweet has been updated!');
 
-    res.send(tweet);
+    // res.send(tweet);
 })
 
 
