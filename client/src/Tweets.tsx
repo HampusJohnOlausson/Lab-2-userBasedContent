93faeca4c09e19337c0b0b735dbac559a3683bab
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-21 16:06:11 +0200

    displaying tweet on page

--- client/src/Tweets.tsx
+++ client/src/Tweets.tsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Tweets extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+  
+    state = {
+        name:'',
+        tweet: '',
+        posts: [],
     }
+
+    componentDidMount = () => {
+        this.getPosts();
+    };
+
+    getPosts = () => {
+        axios.get('/api/posts')
+            .then((response) => {
+                const data = response.data;
+                this.setState({ posts: data })
+                console.log(data);
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Tweets</h1>
+        <div>
+          {this.state.posts.map((post: any, index: any) => (
+            <div key={index}>
+              <h3>{post.name}</h3>
+              <h5>{post.tweet}</h5>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
