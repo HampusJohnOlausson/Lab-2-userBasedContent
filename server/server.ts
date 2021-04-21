0a7b19701a1b06db82c4a17166272cb2059518ce
Oliver Johansson <oliverjohansson3@gmail.com>
2021-04-21 14:16:52 +0200

    User logic

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import userRouter from './routes/users.routes'
 import postRouter from './routes/posts.routes'
+import cookieSession from 'cookie-session'
 import mongoose from 'mongoose'
 
 const app = express();
 
 const PORT = 4000;
 
-const url = "";
+const url = "mongodb://localhost:27017/lab-2";
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
+app.use(cookieSession({
+    name: 'session', 
+    secret: '6krHRZ8P', 
+    secure: false, 
+    maxAge: 10000 * 60, 
+    httpOnly: true 
+}))
 
 mongoose.connect(url,  options)
 .then((result: any) => app.listen(PORT))
