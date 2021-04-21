e1757bc035ca69ddc6c78fd798f33a581bbe4538
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-21 18:09:09 +0200

    added delete function

--- client/src/Tweets.tsx
+++ client/src/Tweets.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import "./Tweet.css";
+import { response } from 'express';
 
 export default class Tweets extends Component {
   
@@ -9,13 +10,15 @@ export default class Tweets extends Component {
         tweet: '',
         posts: [],
         createdAt: '',
+        tweetId: '',
     }
 
     componentDidMount = () => {
         this.getPosts();
+
     };
 
-    getPosts = () => {
+   getPosts = () => {
         axios.get('/api/posts')
             .then((response) => {
                 const data = response.data;
@@ -26,6 +29,20 @@ export default class Tweets extends Component {
             })
     }
 
+    deleteTweet = (id: any) => {
+        axios('api/posts/' + id, {
+            method: 'DELETE'
+        }).then((response) => {
+                console.log('deleted')
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
+    handleClick = () => {
+        this.deleteTweet("608041a24f09358ebf57e09d");
+
+    }
 
 
   render() {
@@ -33,11 +50,15 @@ export default class Tweets extends Component {
       <div className="tweetWrapper">
         <h1 className="titlePage">Tweets</h1>
         <div className="tweetSection">
-          {this.state.posts.map((post: any, index: any) => (
-            <div className="tweetContainer" key={index}>
+          {this.state.posts.map((post: any) => (
+            <div className="tweetContainer" key={post._id}>
               <h3 className="userName">{post.name}</h3>
               <h5 className="tweetParagraph">{post.tweet}</h5>
               <span className="timeAndDate">{post.createdAt}</span>
+              <div className="lowerSection">
+                <button className="editBtn">Edit</button>
+                <button onClick={this.handleClick} className="deleteBtn">Delete</button>
+              </div>
             </div>
           ))}
         </div>
