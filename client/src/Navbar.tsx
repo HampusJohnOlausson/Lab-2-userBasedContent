820f7490ab3b8bf59ec79797c22c9d7413ba1432
Oliver Johansson <oliverjohansson3@gmail.com>
2021-04-29 11:32:14 +0200

    Added context to navbar and general tweets

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,40 +1,16 @@
 import axios from 'axios'
-import React, { CSSProperties, useEffect, useState } from 'react'
+import React, { CSSProperties, useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import './style/NavBar.css'
 import HomeIcon from '@material-ui/icons/Home';
+import { UserContext } from './Contexts/userContext';
 
 export default function NavBar() {
-    const [username, setUserName] = useState('');
-    const [userRole, setUserRole] = useState('')
+    const userContext = useContext(UserContext)
+
     const history = useHistory()
 
-    const makeRequest = async () => {
-        try {
-            const response = await axios.get('/api/users/loggedIn')
-            const result = response.data
-            setUserName(result.userName)
-        } catch (error) {
-            return error
-        }
-    }
-
-    const roleRequest = async () => {
-        try {
-            const response = await axios.get('/api/users/loggedIn/role')
-            const result = response.data
-            setUserRole(result)
-        } catch (error) {
-            return error
-        }
-    }
-
-    useEffect(() => {
-        makeRequest()
-        roleRequest()
-    })
-    
     const logOutRequest = async () => {
         try {
             await axios.delete('/api/users/logout')
@@ -47,7 +23,7 @@ export default function NavBar() {
     return (
         <div className="header">
             <div className="avatarContainer">
-                {username ?
+                {userContext.user.userName ?
                 <Link to="/profile">
                     <div className="profilePic"></div>
                 </Link>
@@ -55,8 +31,8 @@ export default function NavBar() {
                     <div className="profilePic"></div>
                 }
                 <div className="userName">
-                    {username ? 
-                    <h3>{username}</h3>
+                    {userContext.user.userName ? 
+                    <h3>{userContext.user.userName}</h3>
                     :
                     <Link to="/userSelect"><h4>Sign in...</h4></Link>
                     }
@@ -68,7 +44,7 @@ export default function NavBar() {
                 </Link> 
             </div>
             <div className="logoutContainer">
-                {userRole === 'admin' ?
+                {userContext.user.role === 'admin' ?
                 <Link to="/admin">
                     <p>Admin</p>
                 </Link>
@@ -76,7 +52,7 @@ export default function NavBar() {
                     <>
                     </>
                 }
-                {username? 
+                {userContext.user.userName? 
                 <button onClick={logOutRequest}>Log out</button>
                 :
                 <div className="userNavigation">
